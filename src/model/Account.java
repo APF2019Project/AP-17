b4ab62f892d1b95e1dc9e9b48d7a3c1255a024e7
Arman <rman.mo2000@gmai.com>
2019-12-08 13:53:42 +0330

    Account class added

---
+++ src/model/Account.java
@@ -0,0 +1,64 @@
+package model;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class Account implements Serializable {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    private static int idCounter = 0;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.id = idCounter;
+        idCounter++;
+        accounts.add(this);
+    }
+
+    //    private final String name;
+    private String username;
+    private String password;
+    private int id;
+    private int gameCoins = 0;
+    private int shopCoins = 0;
+    private Collection collection;
+    private int zombiesKilled = 0;
+
+    public boolean passwordMatches(String password) {
+        if (this.password.equals(password)) {
+            return true;
+        }
+        return false;
+    }
+    public void addToJson(){
+        Gson json = new Gson();
+        String objectToJson = json.toJson(this);
+        System.out.println(objectToJson);
+    }
+    public static void saveAccount() throws FileNotFoundException {
+//        Type accountType = new TypeToken<ArrayList<Account>>(){}.getType();
+        System.out.println(accounts.size());
+        Gson json = new Gson();
+        String objectToJson = json.toJson(accounts);
+        System.out.println(objectToJson);
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream("accounts.txt"));
+        printWriter.write(objectToJson);
+        printWriter.close();
+    }
+
+
+    public static ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
