229f975cd32b870a5ba17d7ca3cae907cf2603f2
amir mahdi hosseinabadi <hosseinabadi26@gmail.com>
2019-12-26 12:38:31 +0330

    Show Shop function created

--- src/model/Collection.java
+++ src/model/Collection.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Collection {
     private Account owner;
     private ArrayList<Card> cards = new ArrayList<>();
+    
 
     public ArrayList<Card> getCards() {
         return cards;
