adc789cbd58b423015d975aee654a6506c138b00
Arman <rman.mo2000@gmai.com>
2019-12-28 11:25:37 +0330

    class AI created

---
+++ src/model/AI.java
@@ -0,0 +1,17 @@
+package model;
+
+public class AI extends Player{
+
+
+    public void preProcess(){
+
+    }
+    public void play(){
+
+    }
+
+
+    public AI(Account account) {
+        super(account);
+    }
+}
