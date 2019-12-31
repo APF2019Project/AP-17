88b2c6fc2c0ec9d085148356cdf62bf1ca272d9d
Arman <rman.mo2000@gmai.com>
2019-12-31 13:25:44 +0330

    CherryBomb shoot function completed

--- src/control/Action/CherryBomb.java
+++ src/control/Action/CherryBomb.java
@@ -1,4 +1,74 @@
 package control.Action;
 
-public class CherryBomb {
+import control.BattleClasses.Cell;
+import model.Plant;
+
+public class CherryBomb extends Action{
+
+
+    @Override
+    protected void shoot() {
+        if (hasZombieInFront()){
+            Cell cell = plant.getLocation();
+            cell.getZombies().clear();
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() - 1][plant.getLocation().getY()][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() + 1][plant.getLocation().getY()][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX()][plant.getLocation().getY() - 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX()][plant.getLocation().getY() + 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() - 1][plant.getLocation().getY() - 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() - 1][plant.getLocation().getY() + 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() + 1][plant.getLocation().getY() - 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+            try {
+                cell = plant.getMap().getCells()[plant.getLocation().getX() + 1][plant.getLocation().getY() + 1][0];
+                cell.getZombies().clear();
+            } catch (Exception e){
+
+            }
+
+        }
+    }
+
+    @Override
+    protected boolean hasZombieInFront() {
+        return true;
+    }
+    public CherryBomb(Plant plant) {
+        super(plant);
+    }
+
 }
