08611887a96a906ec4765f2600401bfb48c4a459
Arman <rman.mo2000@gmai.com>
2019-12-31 13:32:16 +0330

    shoot function in Jalapeno class completed

--- src/control/Action/Jalapeno.java
+++ src/control/Action/Jalapeno.java
@@ -1,4 +1,28 @@
 package control.Action;
 
-public class Jalapeno {
+import control.BattleClasses.Cell;
+import control.BattleClasses.Map;
+import model.Plant;
+
+public class Jalapeno extends Action{
+
+
+    @Override
+    protected void shoot() {
+     if (hasZombieInFront()){
+         Cell[][] row = plant.getMap().getCells()[plant.getLocation().getX()];
+         for (int i = 0; i < Map.getWidth(); i++) {
+             Cell cell  = row[i][0];
+             cell.getZombies().clear();
+         }
+     }
+    }
+
+    @Override
+    protected boolean hasZombieInFront() {
+        return true;
+    }
+    public Jalapeno(Plant plant) {
+        super(plant);
+    }
 }
